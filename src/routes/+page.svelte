5e1d191f908af1ba78c9c9849fc35e039302ff41
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2023-03-22 11:22:10 +0200

    Basic todo done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let tasks: string[] = [];
+
+    function handleNewTask() {
+        tasks = [...tasks, "New Task"];
+    }
+
+    function handleDeleteTask(index: number) {
+        tasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)]
+    }
+
+    function handleTaskChange(index: number, value: string) {
+        tasks = [...tasks.slice(0, index), value, ...tasks.slice(index + 1)]
+    }
+    
+</script>
+
+<h1>Simple Todo App</h1>
+<button on:click={handleNewTask}>New Task</button>
+<ul>
+    {#each tasks as task, index}
+        <li>
+            <input bind:value={task} on:change={() => handleTaskChange(index, task)}>
+            <button on:click={() => handleDeleteTask(index)}>Delete</button>
+        </li>
+    {/each}
+</ul>
